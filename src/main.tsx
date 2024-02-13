3a11f03d899e7a0fa4cc17dfc624fd5b297d6438
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-02-14 04:53:47 +0530

    changes to navbar

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { VideoContextProvider } from "./context/videosContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <VideoContextProvider>
+        <App />
+      </VideoContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );

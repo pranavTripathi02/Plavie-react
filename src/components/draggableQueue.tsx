9c106b5db8e5954cd59ea5b5bb188fe36184bdb4
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-02-15 21:18:44 +0530

    ordering fixes still remaining

--- src/components/draggableQueue.tsx
+++ src/components/draggableQueue.tsx
@@ -2,7 +2,7 @@
 import DragSVG from "../assets/drag.svg?react";
 import baseUrl from "../api/baseUrl";
 import "./draggableQueue.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import usePlaylistContext from "../hooks/usePlaylistContext";
 import useVideosContext from "../hooks/useVideosContext";
@@ -12,6 +12,7 @@ function DraggableQueue() {
   const { currentPlaylist, setCurrentPlaylist, updatePlaylistOrder } =
     usePlaylistContext();
   const playlistQueue = currentPlaylist?.playlistContents;
+  const navigate = useNavigate();
 
   const { videosList } = useVideosContext();
   const [draggingItemIdx, setDraggingItemIdx] = useState<number | null>(null);
@@ -19,6 +20,7 @@ function DraggableQueue() {
   const currentPlaylistVideos = videosList.filter(
     (video) => currentPlaylist?.playlistContents?.includes(video.id),
   );
+  // console.log(currentPlaylistVideos);
 
   if (!currentPlaylist?.playlistId) {
     return <div>No playlist selected</div>;
@@ -31,7 +33,6 @@ function DraggableQueue() {
     e: React.DragEvent<HTMLDivElement>,
     str: "start" | "end",
   ) => {
-    // console.log(e.currentTarget);
     if (str === "start") {
       e.currentTarget.closest(".queueItem")?.classList.add("draggingOver");
     } else {
@@ -61,7 +62,6 @@ function DraggableQueue() {
       .closest(".queueItem")
       ?.querySelector(".queue-index")?.innerHTML;
     const newIdx = parseInt(newIdxString!);
-    // console.log(newIdx, draggingItem);
 
     const newQueue = playlistQueue!.map((playlistItem) => {
       if (playlistItem === draggingItemIdx) playlistItem = newIdx;
@@ -79,67 +79,69 @@ function DraggableQueue() {
 
   return (
     <div>
-      {currentPlaylistVideos.map((video) => (
-        <div
-          className="queueItem"
-          key={video.id}
-          draggable={true}
-          // dragover used to recheck hover
-          // onDragOver={(e) => handleDragBehind(e, "start")}
-          onDragLeave={(e) => handleDragBehind(e, "end")}
-          onDragStart={(e) => handleDrag(e, "start")}
-          onDragEnd={(e) => handleDrag(e, "end")}
-          onDragOver={(e) => e.preventDefault()}
-          onDrop={(e) => handleDrop(e)}
-        >
-          <Link
-            to={`/watch?vid=${video.id}`}
-            onClick={() => {
-              // setCurrentPlaylist({
-              //   ...currentPlaylist,
-              //   playlistIdx:
-              //     (currentPlaylist.playlistContents?.indexOf(video.id) || 0) +
-              //     1,
-              // });
-
-              console.log(
-                currentPlaylist,
-                (currentPlaylist.playlistContents?.indexOf(video.id) || 0) + 1,
-              );
-            }}
+      {currentPlaylistVideos.map((video) => {
+        const currentIdx =
+          (currentPlaylist.playlistContents?.indexOf(video.id) || 0) + 1;
+        return (
+          <div
+            className="queueItem"
+            key={video.id}
+            draggable={true}
+            // dragover used to recheck hover
+            // onDragOver={(e) => handleDragBehind(e, "start")}
+            onDragLeave={(e) => handleDragBehind(e, "end")}
+            onDragStart={(e) => handleDrag(e, "start")}
+            onDragEnd={(e) => handleDrag(e, "end")}
+            onDragOver={(e) => e.preventDefault()}
+            onDrop={(e) => handleDrop(e)}
           >
             <div
-              className={`relative flex gap-8 items-center my-2 ${
-                currentPlaylist.playlistIdx === video.id
-                  ? "bg-[var(--text)] text-[var(--bg)]"
-                  : "bg-[var(--secondary-2)] text-[var(--text)]"
-              } rounded-md px-4 select-none`}
+              // to={`/watch?vid=${video.id}&pid=${currentPlaylist.playlistId}&pidx=${currentPlaylist.playlistIdx}`}
+              onClick={() => {
+                setCurrentPlaylist({
+                  ...currentPlaylist,
+                  playlistIdx:
+                    (currentPlaylist.playlistContents?.indexOf(video.id) || 0) +
+                    1,
+                });
+                navigate(
+                  `/watch?vid=${video.id}&pid=${currentPlaylist.playlistId}&pidx=${currentIdx}`,
+                );
+              }}
             >
-              <div className="flex gap-4 items-center z-20">
-                {/* drag video */}
-                <div className="cursor-grabbing me-2">
-                  <span className="hidden queue-index">{video.id}</span>
-                  <DragSVG />
+              <div
+                className={`relative flex gap-8 items-center my-2 ${
+                  currentIdx === currentPlaylist.playlistIdx
+                    ? "bg-[var(--text)] text-[var(--bg)]"
+                    : "bg-[var(--secondary-2)] text-[var(--text)]"
+                } rounded-md px-4 select-none`}
+              >
+                <div className="flex gap-4 items-center z-20">
+                  {/* drag video */}
+                  <div className="cursor-grabbing me-2">
+                    <span className="hidden queue-index">{video.id}</span>
+                    <DragSVG />
+                  </div>
+                  {/* video thumbnail */}
+                  <div>
+                    <img
+                      src={`${baseUrl}${video.thumb}`}
+                      alt="video thumbnail"
+                      width="100"
+                    />
+                  </div>
+                  {/* video title */}
+                  <div>
+                    <span>{video.title}</span>
+                  </div>
+                  {/* for consistent styling */}
+                  {/* <div className="absolute left-0 right-0 top-0 bottom-0"></div> */}
                 </div>
-                {/* video thumbnail */}
-                <div>
-                  <img
-                    src={`${baseUrl}${video.thumb}`}
-                    alt="video thumbnail"
-                    width="100"
-                  />
-                </div>
-                {/* video title */}
-                <div>
-                  <span>{video.title}</span>
-                </div>
-                {/* for consistent styling */}
-                {/* <div className="absolute left-0 right-0 top-0 bottom-0"></div> */}
               </div>
             </div>
-          </Link>
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/pages/playlists.tsx
+++ src/pages/playlists.tsx
@@ -1,19 +1,30 @@
 import { useNavigate } from "react-router-dom";
 import usePlaylistContext from "../hooks/usePlaylistContext";
 import DraggableQueue from "../components/draggableQueue";
+import { useState } from "react";
+import { CreateNewPlaylist } from "../components/addToPlaylistsModal";
 // import UnderDevelopment from "./underDevelopment";
 
 function Playlists() {
-  const { playlists, currentPlaylist, setCurrentPlaylist } =
+  const { playlists, currentPlaylist, setCurrentPlaylist, addPlaylist } =
     usePlaylistContext();
 
+  const [createNewPlaylist, setCreateNewPlaylist] = useState(false);
+  const changeCreateNewPlaylistStatus = () => {
+    setCreateNewPlaylist((prev) => !prev);
+  };
+  const handleAddNewPlaylist = (playlistName: string) => {
+    if (!playlistName || playlistName.length < 1) return;
+    addPlaylist({ playlistName });
+    setCreateNewPlaylist(false);
+  };
   setCurrentPlaylist(null);
   const navigate = useNavigate();
   const handleChoosePlaylist = (playlistId: number) => {
     setCurrentPlaylist(
       playlists.find((playlist) => playlist.playlistId === playlistId) || null,
     );
-    navigate(`/watch?playlist=${playlistId}`);
+    navigate(`/watch?pid=${playlistId}&pidx=${currentPlaylist?.playlistIdx}`);
   };
   return (
     <div>
@@ -36,6 +47,18 @@ function Playlists() {
           );
         })
       )}
+      {/* create new playlist */}
+      <div className="w-full flex justify-end">
+        <button
+          className="mx-4 my-2 px-2 py-1 rounded-lg bg-[var(--secondary-2)]"
+          onClick={changeCreateNewPlaylistStatus}
+        >
+          {createNewPlaylist ? "Cancel" : "Create new playlist"}
+        </button>
+        {createNewPlaylist && (
+          <CreateNewPlaylist handleAddNewPlaylist={handleAddNewPlaylist} />
+        )}
+      </div>
     </div>
   );
 }
